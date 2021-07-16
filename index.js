54f998608b6b125dc897aed90b831b14ac1e9b9f
John Osterfeld <jczarfeld@gmail.com>
2021-07-16 09:55:25 -0400

    second commit with initial termimal prompts

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+{
+type: "input",
+name : "github",
+message:"What is your Github username?"
+},
+{
+ type: "input",
+ name : "email",
+ message: "What is your e-mail address?"   
+}
+
+
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), ))
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(() => {
+        console.log(answers);
+    
+    })
+}
 
 // Function call to initialize app
 init();
