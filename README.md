590fabec9a5056a71c8ffd10a87a91752a23a3a7
John Osterfeld <jczarfeld@gmail.com>
2021-07-16 20:36:02 -0400

    final commit for now, need to figure out getting the image into my project readme file

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # readMe_gen
-readme generator
-is designed to prompt users to enter useful information regarding their project. The user's inputs will be attached to another text file displaying the results. 
+Hi! My name is John Osterfeld
+The title of my project is readMe_gen.
+
+Here is more information
+Description: Reame file generator
+Installation instructions: install the node packages and run the index.js file in terminal
+Usage Information: Answer the questions in the terminal and you'll have a readme index file
+Contribution guidelines: Please feel free to fork and upgrade it!
+Test instructions: Just try it and enjoy!
+I am still alive! Contact Me!
+My GitHub username is jczarfeld
+LinkedIn: undefined
--- index.js
+++ index.js
@@ -4,68 +4,104 @@ const inquirer = require("inquirer");
 const path = require("path");
 const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [
 
-{
-type: "input",
-name : "github",
-message:"What is your Github username?"
-},
-{
- type: "input",
- name : "email",
- message: "What is your e-mail address?"   
-}
-{
-    type: "input",
-    name : "title",
-    message:"What is your project Title?"
-    },
-    {
-     type: "input",
-     name : "description",
-     message: "Please describe your project"   
-    }
-    {
-        type: "input",
-        name : "instructions",
-        message:"Please list specific installation instructions"
+
+
+// html for the readme file
+const generateHTML = (answers) =>
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./style.css">
+ 
+
+  <title>My New Read Me</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">The title of my project is ${answers.title}.</p>
+    <h3>Here is more information</h3>
+    <ul class="list-group">
+      <li class="list-group-item">Description: ${answers.description}</li>
+      <li class="list-group-item">Installation instructions: ${answers.instructions}</li>
+      <li class="list-group-item">Usage Information: ${answers.information}</li>
+      <li class="list-group-item">Contribution guidelines: ${answers.guidelines}</li>
+      <li class="list-group-item">Test instructions: ${answers.test}</li>
+    </ul>
+    <h3>I am still alive! Contact Me!</h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+// questions in the terminal for user input
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+          },
+
+        {
+            type: "input",
+            name: "github",
+            message: "What is your Github username?"
         },
         {
-         type: "input",
-         name : "information",
-         message: "Please list specific usage information"   
-        }
+            type: "input",
+            name: "email",
+            message: "What is your e-mail address?"
+        },
         {
             type: "input",
-            name : "guidelines",
-            message: "How could one contribute to your project?"   
-           }
-           {
+            name: "title",
+            message: "What is your project Title?"
+        },
+        {
             type: "input",
-            name : "test",
-            message: "Please list specific test instructions"   
-           }
-            
-            
+            name: "description",
+            message: "Please describe your project"
+        },
+        {
+            type: "input",
+            name: "instructions",
+            message: "Please list specific installation instructions"
+        },
+        {
+            type: "input",
+            name: "information",
+            message: "Please list specific usage information"
+        },
+        {
+            type: "input",
+            name: "guidelines",
+            message: "How could one contribute to your project?"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Please list specific test instructions"
+        },
 
 
-];
+        //takes user input and prints onto the html layout onto a new file
 
+    ])
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), ))
-}
-
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then(() => {
-        console.log(answers);
-    
-    })
-}
-
-// Function call to initialize app
-init();
+        fs.writeFile('index.html', htmlPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+    });
